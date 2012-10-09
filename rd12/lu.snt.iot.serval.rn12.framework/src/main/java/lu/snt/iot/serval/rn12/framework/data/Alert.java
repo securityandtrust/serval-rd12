5d131ca46b7fcc9b336cbbd0cd8240d7ff8c0d85
Grégory NAIN <gregory.nain@gmail.com>
2012-10-09 17:58:04 +0200

    Creating a framework project for utility data structures

---
+++ rd12/lu.snt.iot.serval.rn12.framework/src/main/java/lu/snt/iot/serval/rn12/framework/data/Alert.java
@@ -0,0 +1,13 @@
+package lu.snt.iot.serval.rn12.framework.data;/*
+* Author : Gregory Nain (developer.name@uni.lu)
+* Date : 09/10/12
+* (c) 2012 University of Luxembourg – Interdisciplinary Centre for Security Reliability and Trust (SnT)
+* All rights reserved
+*/
+
+public class Alert {
+
+
+
+
+}
