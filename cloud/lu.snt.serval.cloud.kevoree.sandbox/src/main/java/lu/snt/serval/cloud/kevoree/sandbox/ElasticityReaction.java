4fa9d22bcb214dec92f4e64b43d1841eab59ea2b
François Fouquet <fouquet.f@gmail.com>
2012-11-08 17:32:45 +0100

    main file

---
+++ cloud/lu.snt.serval.cloud.kevoree.sandbox/src/main/java/lu/snt/serval/cloud/kevoree/sandbox/ElasticityReaction.java
@@ -0,0 +1,17 @@
+package lu.snt.serval.cloud.kevoree.sandbox;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 08/11/12
+ * Time: 17:30
+ */
+public class ElasticityReaction {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello");
+
+    }
+
+}
